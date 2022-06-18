eca91d1f2dc6b05181ed285d010623370ff340fc
daniazzam <dani.azzam@lau.edu>
2022-06-18 20:18:56 +0300

    Added post message function using Twilio.rest API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-
-from dotenv import load_dotenv
+from twilio.rest import Client
+#from dotenv import load_dotenv
 from flask import (
     Flask,
     flash, 
@@ -10,11 +10,15 @@ from flask import (
     url_for,
 )
 
-load_dotenv()
+#load_dotenv()
 app = Flask(__name__)
 app.secret_key = "ssssh don't tell anyone"
 
 TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
+
+account_sid = 'ACa25ddc420f9aa809969b283d3fb45d27' 
+auth_token = '61b934a9fd66ed43e88aa673382bbe61' 
+client = Client(account_sid, auth_token) 
 
 def get_sent_messages():
     # TODO: Make this return a collection of messages that were sent from the number
@@ -22,8 +26,11 @@ def get_sent_messages():
     return messages
 
 def send_message(to, body):
-    # TODO: Send the text message
-    pass
+    client.messages.create( 
+                            from_='whatsapp:+14155238886',  
+                            body=body,      
+                            to=to 
+                        ) 
 
 @app.route("/", methods=["GET"])
 def index():
@@ -35,7 +42,8 @@ def add_compliment():
     sender = request.values.get('sender', 'Someone')
     receiver = request.values.get('receiver', 'Someone')
     compliment = request.values.get('compliment', 'wonderful')
-    to = request.values.get('to')
+    to1 = request.values.get('to')
+    to='whatsapp:'+to1
     body = f'{sender} says: {receiver} is {compliment}. See more compliments at {request.url_root}'
     send_message(to, body)
     flash('Your message was successfully sent')
